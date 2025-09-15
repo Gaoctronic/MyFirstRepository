b120e29d3b9d54c7017c6972604f663500a2bd2e
Yunqiao Zhang <uxwus@student.kit.edu>
2025-09-15 21:36:39 +0800

    关于循环的一些练习

---
+++ learn_loop.c
@@ -0,0 +1,24 @@
+// 2025年9月25日于208
+#include <stdio.h>
+
+int main(void)
+{
+    for (int i = 0; i <= 1000; i++)
+    {
+        int number = i;
+        int count = 0;
+
+        while ((number / 10) != 0)
+        {
+            count = count + (number % 10);
+            number = number / 10;
+        }
+
+        if (count + number == 15)
+        {
+            printf("%d\n", i);
+        }
+    }
+
+    return 0;
+}
---
+++ loop_calculator.c
@@ -0,0 +1,20 @@
+// 2025年9月25日于208
+#include <stdio.h>
+
+int main(void)
+{
+    long long sum = 0;
+    for (int i = 1; i <= 10; i++)
+    {   
+        long long power = i;
+        for (int j = 0; j < i; j++)
+        {
+            power = power * i;
+        }
+
+        sum = sum + power;
+    }
+
+    printf("The final sum is: %lld\n", sum);
+    return 0;
+}
